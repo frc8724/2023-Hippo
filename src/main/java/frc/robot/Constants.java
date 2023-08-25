e428bdf67f54ff90c6bc4c47aeda04f6166106aa
Ryan Owens <583078+owensrd@users.noreply.github.com>
2023-08-24 20:59:34 -0400

    Change to the constants file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,15 +23,15 @@ public final class Constants {
   }
 
   public static class DriveConstants {
-    public static final int kFrontLeftDriveMotorPort = 0;
-    public static final int kRearLeftDriveMotorPort = 2;
-    public static final int kFrontRightDriveMotorPort = 4;
-    public static final int kRearRightDriveMotorPort = 6;
+    public static final int kFrontLeftDriveMotorPort = 2;
+    public static final int kRearLeftDriveMotorPort = 7;
+    public static final int kFrontRightDriveMotorPort = 5;
+    public static final int kRearRightDriveMotorPort = 1;
 
-    public static final int kFrontLeftTurningMotorPort = 1;
-    public static final int kRearLeftTurningMotorPort = 3;
-    public static final int kFrontRightTurningMotorPort = 5;
-    public static final int kRearRightTurningMotorPort = 7;
+    public static final int kFrontLeftTurningMotorPort = 3;
+    public static final int kRearLeftTurningMotorPort = 6;
+    public static final int kFrontRightTurningMotorPort = 4;
+    public static final int kRearRightTurningMotorPort = 0;
 
     public static final int[] kFrontLeftTurningEncoderPorts = new int[] {0, 1};
     public static final int[] kRearLeftTurningEncoderPorts = new int[] {2, 3};
