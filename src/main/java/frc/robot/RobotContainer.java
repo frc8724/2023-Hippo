6fa4dbbe1b5520f448fcb4feaa4030f5ab1f64c6
Robert Deml <rdeml31@gmail.com>
2023-09-14 19:12:35 -0400

    disable drive subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,7 +38,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
 	// The robot's subsystems and commands are defined here...
-	private final DriveBaseSubsystem m_robotDrive = new DriveBaseSubsystem();
+	// private final DriveBaseSubsystem m_robotDrive = new DriveBaseSubsystem();
 	// private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
 	private final SimpleFalconSubsystem m_simpleSubsystem = new SimpleFalconSubsystem("test", Constants.DriveConstants.kFrontLeftDriveMotorPort);
