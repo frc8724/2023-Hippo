00c2ca9aaa2e4670e779bb48bbaa64be3f9f8c13
Driverstation Laptop <mayhem@mayhemrobotics.com>
2023-11-02 20:59:52 -0400

    Minor tweak

--- src/main/java/frc/robot/subsystems/Limelight/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/Limelight/LimelightSubsystem.java
@@ -11,27 +11,36 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tv;
-    NetworkTableEntry tx;
-    NetworkTableEntry ty;
-    NetworkTableEntry ta;
+    NetworkTableEntry tv; // Whether the limelight has any valid targets (0-1)
+    NetworkTableEntry tx; // Horizontal Offset from Crosshair to target (-27 degrees to 27 degrees)
+    NetworkTableEntry ty; // Vertical Offset From Crosshair to target (-20.5 degrees to 20.5 degrees)
+    NetworkTableEntry ta; // Target Area (0% of image to 100% of image)
+    NetworkTableEntry ts; // Skew or Rotation (-90 degrees to 0 degrees)
+    NetworkTableEntry tl; // The pipeline's latency contribution (ms) Add at least 11ms for image capture
+                          // latency.
+    NetworkTableEntry tshort; // Sidelength of shortest side of the fitted bounding box (pixels)
+    NetworkTableEntry tlong; // Sidelength of longest side of the fitted bounding box (pixels)
+    NetworkTableEntry thoriz; // Horizontal sidelength of the rough bounding box (0-320 pixels)
+    NetworkTableEntry tvert; // Vertical sidelength of the rough bounding box (0-320 pixels)
 
     public LimelightSubsystem() {
         tv = table.getEntry("tv");
         tx = table.getEntry("tx");
         ty = table.getEntry("ty");
         ta = table.getEntry("ta");
+        ts = table.getEntry("ts");
+        tl = table.getEntry("tl");
+        tlong = table.getEntry("tshort");
+        tlong = table.getEntry("tlong");
+        thoriz = table.getEntry("thoriz");
+        tvert = table.getEntry("tvert");
     }
 
     @Override
     public void periodic() {
-        double hasImage = tv.getDouble(0.0);
-        double x = tx.getDouble(0.0);
-        double y = ty.getDouble(0.0);
-        double area = ta.getDouble(0.0);
-        SmartDashboard.putNumber("LimelightImage?", hasImage);
-        SmartDashboard.putNumber("LimelightX", x);
-        SmartDashboard.putNumber("LimelightY", y);
-        SmartDashboard.putNumber("LimelightArea", area);
+        SmartDashboard.putNumber("LimelightImage?", tv.getDouble(0.0));
+        SmartDashboard.putNumber("LimelightX", tx.getDouble(0.0));
+        SmartDashboard.putNumber("LimelightY", ty.getDouble(0.0));
+        SmartDashboard.putNumber("LimelightArea", ta.getDouble(0.0));
     }
 }
